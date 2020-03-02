2894737fec83fad9fa747373296b4365134d9abc
saurabh251996 <61679299+saurabh251996@users.noreply.github.com>
2020-03-02 13:25:46 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://learnest:learnest@ds263707.mlab.com:63707/crudtodo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://127.0.0.1:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
